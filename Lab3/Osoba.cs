16231d5b38db7973fd12f1cb9ed80c7f8e8e7ad8
katarzynanosal6 <katarzynan101@wp.pl>
2026-04-09 12:21:00 +0200

    dodanie class

---
+++ Lab3/Osoba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Osoba
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Wiek { get; set; }
+        public string Stanowisko { get; set; }
+
+        public Osoba() { }
+
+        public Osoba(int id, string imie, string nazwisko, int wiek, string stanowisko)
+        {
+            Id = id;
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Wiek = wiek;
+            Stanowisko = stanowisko;
+        }
+    }
+}
